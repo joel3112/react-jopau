56282d957b6b2add4223d137f7b995a252a4a126
Joel Paucarima Franco <joel.dev.3112@outlook.es>
2022-10-02 23:06:18 +0200

    chore(utils): setup + install dependencies

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,6 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
     '@jopau-react/utils': '<rootDir>/packages/utils/src',
-    '@jopau-react/utils/(.*)$': '<rootDir>/packages/utils/src/$1',
-    '@jopau-react/hooks/(.*)$': '<rootDir>/packages/hooks/src/$1'
+    '@jopau-react/utils/(.*)$': '<rootDir>/packages/utils/src/$1'
   }
 };
