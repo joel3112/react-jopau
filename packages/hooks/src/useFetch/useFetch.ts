668bd4bdd50546cff327c1a890c1a968a70cc267
Joel Paucarima Franco <joel.dev.3112@outlook.es>
2022-10-28 03:10:57 +0200

    refactor(hooks)!: add options in useFetch

--- packages/hooks/src/useFetch/useFetch.ts
+++ packages/hooks/src/useFetch/useFetch.ts
@@ -1,20 +1,53 @@
-import useSWR, { BareFetcher, KeyedMutator } from 'swr';
+import useSWR, { Fetcher } from 'swr';
+import axios from 'axios';
+import { getPropValue } from '@react-jopau/utils/object';
 
-export type UseFetch<T> = {
-  data: Array<T> | T | null;
-  loading: boolean;
-  error: { status_code: number; status_message: string } | null;
-  mutate?: KeyedMutator<T>;
+type useFetchData<T> = Array<T> | T | null;
+type useFetchError<U> = U | null;
+
+type UseFetchOptions<T, U> = {
+  method?: string;
+  params?: Record<string, string>;
+  headers?: Record<string, string>;
+  body?: Record<string, string>;
+  onSuccess?: (data: unknown) => useFetchData<T>;
+  onError?: (error: unknown) => useFetchError<U>;
 };
 
-export const useFetch = <T>(path: string | false | null, fetcher: BareFetcher<T>): UseFetch<T> => {
-  const { data, error, mutate } = useSWR<T>(path, fetcher);
+export type UseFetch<T, U> = {
+  data: useFetchData<T>;
+  loading: boolean;
+  error: useFetchError<U>;
+};
+
+export const useFetch = <T, U = {}>(
+  path: string | false | null,
+  options?: UseFetchOptions<T, U>
+): UseFetch<T, U> => {
+  console.log('useFetch', path, options);
+  const fetcher = () => {
+    return axios
+      .request({
+        url: path || '',
+        method: getPropValue(options, 'method', 'get'),
+        params: getPropValue(options, 'params', {}),
+        headers: getPropValue(options, 'headers', {}),
+        data: getPropValue(options, 'body', {})
+      })
+      .then(getPropValue(options, 'onSuccess', (res) => res.data))
+      .catch(
+        getPropValue(options, 'onError', (error) => {
+          throw error;
+        })
+      );
+  };
+
+  const { data, error } = useSWR<T, U>(path, fetcher as unknown as Fetcher<T>);
   const loading = !error && !data;
 
   return {
     data: data as T,
     loading,
-    error,
-    mutate: mutate as KeyedMutator<T>
+    error: error as U
   };
 };
