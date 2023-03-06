68089aac901894664105375f6064ed444db0f0ec
Joel Paucarima Franco <joel.dev.3112@gmail.com>
2023-03-06 01:16:41 +0100

    feat: add breakpoint storybook addon

--- .storybook/decorators.js
+++ .storybook/decorators.js
@@ -1,18 +1,19 @@
 /* eslint-disable react/prop-types */
 import * as React from 'react';
 import { addons } from '@storybook/addons';
-import { useLocalStorage } from '@react-jopau/hooks';
+import { useBreakpoint, useLocalStorage } from '@react-jopau/hooks';
 import { ThemeProvider } from '@react-jopau/components';
 import { DARK_MODE_STORAGE_KEY, THEME_SELECTOR_STORAGE_KEY, themes } from '@react-jopau/styles';
 
 const ThemeProviderMemo = React.memo(ThemeProvider);
 
 const ThemeStoryProvider = ({ children }) => {
+  const channel = addons.getChannel();
   const [themeKey, setThemeKey] = useLocalStorage(THEME_SELECTOR_STORAGE_KEY, 'default');
   const [colorScheme, setColorScheme] = useLocalStorage(DARK_MODE_STORAGE_KEY, 'light');
+  const { key } = useBreakpoint();
 
   React.useEffect(() => {
-    const channel = addons.getChannel();
     channel.on('theme-selected', setThemeKey);
     channel.on('color-scheme-selected', setColorScheme);
     channel.emit('story-mounted');
@@ -22,6 +23,10 @@ const ThemeStoryProvider = ({ children }) => {
       channel.off('color-scheme-selected', setColorScheme);
     };
   }, []);
+
+  React.useEffect(() => {
+    channel.emit('current-breakpoint-changed', key);
+  }, [key]);
 
   React.useLayoutEffect(() => {
     [window.parent.document.body, document.body].forEach((el) => {
